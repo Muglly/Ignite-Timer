5457b0751704d553c71c3a3b17c2311dd2a8acea
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2023-02-12 09:00:40 -0300

    Add type in form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,15 +7,26 @@ import * as S from './styles'
 
 const newFormCycleValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
-  minutesAmount: zod.number().min(5).max(60),
+  minuteAmount: zod.number().min(5).max(60),
 })
 
+// interface NewCycleFormData {
+//   task: string
+//   minuteAmount: number
+// }
+
+type NewCycleFormData = zod.infer<typeof newFormCycleValidationSchema>
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm({
+  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
     resolver: zodResolver(newFormCycleValidationSchema),
+    defaultValues: {
+      task: '',
+      minuteAmount: 0,
+    },
   })
 
-  function handleCreateNewCycle(data: any) {
+  function handleCreateNewCycle(data: NewCycleFormData) {
     console.log(data)
   }
 
